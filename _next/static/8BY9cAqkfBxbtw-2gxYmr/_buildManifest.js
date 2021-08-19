71bc3d4d9e5f937ec00c0360d14ae825a8b797bf
cyan92128505 <cyan92128505@gmail.com>
2021-08-19 01:31:10 +0000

    Deploying to gh-pages from @ cyan92128505/resume@e05b05e64944c5f230931f4025ab6f6b9a2bdb1f 🚀

--- _next/static/8BY9cAqkfBxbtw-2gxYmr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/68-689af2eb5ee6284a0d30.js","static/chunks/pages/index-d641c1c868b8c891578e.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/zkEo9rCRPShxqHKaFpOco/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/68-689af2eb5ee6284a0d30.js","static/chunks/pages/index-4eb31c327fc44e0baa3a.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
