8faa5509230e0683a37266f51c015cf1dc0d610b
cyan92128505 <cyan92128505@gmail.com>
2021-09-24 10:27:55 +0800

    :bug: Fix GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,9 +15,12 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
           <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-L078JKZ3YZ"
+          ></script>
+          <script
             dangerouslySetInnerHTML={{
               __html: `<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-L078JKZ3YZ"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
