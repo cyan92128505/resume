411c3498c369d2e180fd8ea92f0e80dec45df3a1
cyan92128505 <cyan92128505@gmail.com>
2022-07-02 07:28:53 +0000

    Deploying to gh-pages from @ cyan92128505/resume@7e65da2680654fafb2d11e4890f7c85f3dc64e36 🚀

--- _next/static/txf5r779NrkK2iZ0HGeqs/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/185-330619a6193a064a58aa.js","static/chunks/pages/index-7d5d44a339647ca2b88e.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
